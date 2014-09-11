3e931f03f3db577a5bf292cf1b904a52ed43abff
Tobias Heinemann <tobias.heinemann@gmail.com>
2014-09-11 20:53:25 +0200

    Use variadic functions to read lists

--- read_config.c
+++ read_config.c
@@ -1,5 +1,6 @@
 #include "read_config.h"
 
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
@@ -30,9 +31,12 @@ int read_config (const char *input_file, const char *output_file)
   config_setting_t *physics = add_group (root, "physics");
 
   // Boxsize
-  double boxsize[] = {1.0, 1.0, 1.0};
-  read_float_array (physics, "boxsize", &boxsize, 3);
-  printf ("boxsize = [%f,%f,%f]\n", boxsize[0], boxsize[1], boxsize[2]);
+  /* double boxsize[] = {1.0, 1.0, 1.0}; */
+  /* read_float_array (physics, "boxsize", &boxsize, 3); */
+  /* printf ("boxsize = [%f,%f,%f]\n", boxsize[0], boxsize[1], boxsize[2]); */
+  double Lx = 1.0, Ly = 1.0, Lz = 1.0;
+  read_float_list (physics, "boxsize", 3, &Lx, &Ly, &Lz);
+  printf ("Lx = %f, Ly = %f, Lz = %f\n", Lx, Ly, Lz);
 
   // Reynolds number
   double Re = 1.0;
@@ -48,9 +52,9 @@ int read_config (const char *input_file, const char *output_file)
   config_setting_t *code = add_group (root, "code");
 
   // Resolution
-  long resolution[] = {32, 32, 32};
-  read_int_array (code, "resolution", &resolution, 3);
-  printf ("resolution = [%ld,%ld,%ld]\n", resolution[0], resolution[1], resolution[2]);
+  int nx = 32, ny = 32, nz = 32;
+  read_int_list (code, "pansen", 3, &nx, &ny, &nz);
+  printf ("nx = %d, ny = %d, nz = %d\n", nx, ny, nz);
 
   int dealias = 0;
   read_bool (code, "dealias", &dealias);
@@ -83,40 +87,6 @@ config_setting_t *add_group (config_setting_t *parent, const char *name)
   return group;
 }
 
-void read_float (config_setting_t *parent, const char *name, double *value)
-{
-  config_setting_t *setting = config_setting_get_member (parent, name);
-
-  if (setting) {
-    *value = config_setting_get_float (setting);
-    return;
-  }
-
-  setting = config_setting_add (parent, name, CONFIG_TYPE_FLOAT);
-  config_setting_set_float (setting, *value);
-}
-
-void read_float_array (config_setting_t *parent, const char *name, double (*array)[], unsigned int length) {
-
-  unsigned int index;
-
-  config_setting_t *setting = config_setting_get_member (parent, name);
-
-  if (setting) {
-    assert (config_setting_length (setting) == length);
-    for (index = 0; index < length; index++)
-    {
-      (*array)[index] = config_setting_get_float_elem (setting, index);
-    }
-    return;
-  }
-
-  setting = config_setting_add (parent, name, CONFIG_TYPE_ARRAY);
-  for (index = 0; index < length; index++) {
-    config_setting_set_float_elem (setting, -1, (*array)[index]);
-  }
-}
-
 void read_int (config_setting_t *parent, const char *name, long *value)
 {
   config_setting_t *setting = config_setting_get_member (parent, name);
@@ -130,25 +100,17 @@ void read_int (config_setting_t *parent, const char *name, long *value)
   config_setting_set_int (setting, *value);
 }
 
-void read_int_array (config_setting_t *parent, const char *name, long (*array)[], unsigned int length) {
-
-  unsigned int index;
-
+void read_float (config_setting_t *parent, const char *name, double *value)
+{
   config_setting_t *setting = config_setting_get_member (parent, name);
 
   if (setting) {
-    assert (config_setting_length (setting) == length);
-    for (index = 0; index < length; index++)
-    {
-      (*array)[index] = config_setting_get_int_elem (setting, index);
-    }
+    *value = config_setting_get_float (setting);
     return;
   }
 
-  setting = config_setting_add (parent, name, CONFIG_TYPE_ARRAY);
-  for (index = 0; index < length; index++) {
-    config_setting_set_int_elem (setting, -1, (*array)[index]);
-  }
+  setting = config_setting_add (parent, name, CONFIG_TYPE_FLOAT);
+  config_setting_set_float (setting, *value);
 }
 
 void read_bool (config_setting_t *parent, const char *name, int *value)
@@ -175,4 +137,57 @@ void read_string (config_setting_t *parent, const char *name, const char **value
 
   setting = config_setting_add (parent, name, CONFIG_TYPE_STRING);
   config_setting_set_string (setting, *value);
+}
+
+void read_int_list (config_setting_t *parent, const char *name,
+    const unsigned int count, ...) {
+
+  va_list args;
+  unsigned int index;
+
+  va_start (args, count);
+
+  config_setting_t *setting = config_setting_get_member (parent, name);
+
+  if (setting) {
+    assert (config_setting_length (setting) == count);
+    for (index = 0; index < count; index++)
+    {
+      long value = config_setting_get_int_elem (setting, index);
+      *va_arg (args, int*) = (int) value;
+    }
+    return;
+  }
+
+  setting = config_setting_add (parent, name, CONFIG_TYPE_ARRAY);
+  for (index = 0; index < count; index++) {
+    long value = (long) *va_arg (args, int*);
+    config_setting_set_int_elem (setting, -1, value);
+  }
+}
+
+void read_float_list (config_setting_t *parent, const char *name,
+    const unsigned int count, ...) {
+
+  va_list args;
+
+  va_start (args, count);
+
+  config_setting_t *setting = config_setting_get_member (parent, name);
+
+  if (setting) {
+    assert (config_setting_length (setting) == count);
+    for (unsigned int index = 0; index < count; index++)
+    {
+      double value = config_setting_get_float_elem (setting, index);
+      *va_arg (args, double*) = value;
+    }
+    return;
+  }
+
+  setting = config_setting_add (parent, name, CONFIG_TYPE_ARRAY);
+  for (unsigned int index = 0; index < count; index++) {
+    double value = *va_arg (args, double*);
+    config_setting_set_float_elem (setting, -1, value);
+  }
 }
